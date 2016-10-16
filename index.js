31ee8574b54225ee8e71fca8e30952832fc6c162
akrueg <andrewkrueger@hotmail.com>
2016-10-15 22:19:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-'use strict'
-standardInit()
+'use strict';
+
+standardInit();
 
 //This page lives on CodePen & GitHub.  On GitHub, make the blue
 //links, which don't contain <span>s, target _self:
-if (/github/i.test(doc.URL))
-  qsa('a').filter (a=>!a.qs('span')   )
-          .forEach(a=>a.target='_self')
-
-
+if (/github/i.test(doc.URL)) qsa('a').filter(function (a) {
+  return !a.qs('span');
+}).forEach(function (a) {
+  return a.target = '_self';
+});
