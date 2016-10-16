e9d185adc65f7434e9a54a369a37aa7d4cc5f537
akrueg <andrewkrueger@hotmail.com>
2016-10-15 22:24:21 -0400

    Update flex.js

--- sample_flex/flex.js
+++ sample_flex/flex.js
@@ -1,35 +1,63 @@
-'use strict'
-standardInit()
+'use strict';
 
-var continents=['Africa', 'Antarctica', 'Australia', 'Asia', 'Europe', 'NorthAmerica', 'SouthAmerica']
-continents.forEach(continent=>win['svg'+continent].onclick=showList)
-function showList(ev) { // When called by closeList, ev===undefined.
+standardInit();
+
+var continents = ['Africa', 'Antarctica', 'Australia', 'Asia', 'Europe', 'NorthAmerica', 'SouthAmerica'];
+continents.forEach(function (continent) {
+  return win['svg' + continent].onclick = showList;
+});
+function showList(ev) {
+  // When called by closeList, ev===undefined.
   if (ev) {
-    ev.stopPropagation()
-    var thisContinent=ev.currentTarget.id.slice(3) }
-  continents.filter (continent=>continent!==thisContinent)
-            .forEach(continent=>{ // Reset other continents to css values:
-        win['lst'+continent].style.display=''
-        win['svg'+continent].classList.remove('selected') })
-  if (!thisContinent) return
-  win['lst'+thisContinent].style.display=modal.style.display='block'
-  win['svg'+thisContinent].classList.add('selected') } 
+    ev.stopPropagation();
+    var thisContinent = ev.currentTarget.id.slice(3);
+  }
+  continents.filter(function (continent) {
+    return continent !== thisContinent;
+  }).forEach(function (continent) {
+    // Reset other continents to css values:
+    win['lst' + continent].style.display = '';
+    win['svg' + continent].classList.remove('selected');
+  });
+  if (!thisContinent) return;
+  win['lst' + thisContinent].style.display = modal.style.display = 'block';
+  win['svg' + thisContinent].classList.add('selected');
+}
 
 // Closing modal blocks by clicking background or 'x':
-for (var btn of modal.qsa('.btnX'))
-  btn.onclick=closeList
-  
-window.onclick=ev=>{
-  if (!modal.contains(ev.target)) closeList() }
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = modal.qsa('.btnX')[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var btn = _step.value;
+
+    btn.onclick = closeList;
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}
+
+window.onclick = function (ev) {
+  if (!modal.contains(ev.target)) closeList();
+};
 function closeList() {
-  modal.style.display=''
-  showList() }
+  modal.style.display = '';
+  showList();
+}
 
-var inCodePen  = /codepen/i.test(doc.URL),
-    lnkHome    = spanHome.querySelector('a')
-lnkHome.target = inCodePen? '_blank' : '_self'
-
-
-
-
-
+var inCodePen = /codepen/i.test(doc.URL),
+    lnkHome = spanHome.querySelector('a');
+lnkHome.target = inCodePen ? '_blank' : '_self';
